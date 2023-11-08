3802ee0f9d98179bd27d3cf05118777affdf6c3a
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-08 16:22:05 +0100

    refactoring: bonus exercise using forEach

--- Snack-Bonus/js/script.js
+++ Snack-Bonus/js/script.js
@@ -1,31 +1,41 @@
 const numbersArray = [];
 let newNumbersArray = [];
 
-//Creazione array di numeri random
-for (let i = 0; i < 20; i++) {
-    let number = numbersArray[i];
-    let rndNmb = Math.round(Math.random()*100 + 1);
-    number = rndNmb;
- console.log(number);
- numbersArray.push(number);
-}
-
+createRndNumbersArray(numbersArray, 20)
 console.log(numbersArray);
-
-
 createNewArray(numbersArray, 20, 80);
 console.log(newNumbersArray);
+
+
+//********* FUNZIONI *********//
+
+
+/**
+ * FUNZIONE PER CREARE ARRAY DI NUMERI RANDOM
+ */
+
+function createRndNumbersArray(array, a) {
+    for (let i = 0; i < a; i++) {
+        let number = array[i];
+        let rndNmb = Math.round(Math.random()*100 + 1);
+        number = rndNmb;
+     console.log(number);
+     array.push(number);
+    }}
 
 /**
  * FUNZIONE PER CREARE NUOVO ARRAY CON SOLI NUMERI COMPRESI TRA LE DUE VARIABILI
  * @param {Array, number,number}
  * @returns {Array}
  */
+
 function createNewArray(array, a, b) {
-    for (let i = 0; i < array.length; i++) {
-        const number = array[i];
-        if (number > a && number < b) {
-            newNumbersArray.push(number);
-        }
-        }
+   array.forEach(number => {
+    if (number > a && number < b) {
+        newNumbersArray.push(number);
+    }
+   });
 }
+
+
+
